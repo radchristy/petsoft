717cd5e19a8a587afb59c7dd9da3f97e61f49117
Christine Hong <hongchristine07@gmail.com>
2025-05-04 20:49:17 -0400

    Add Shadcn and homepage buttons

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,5 +1,7 @@
 import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -20,7 +22,14 @@ export default function Home() {
           Use Petsoft to easily keep track of pets under your care. Get Lifetime
           access for $299
         </p>
-        <div className="mt-10"></div>
+        <div className="mt-10 space-x-3">
+          <Button asChild>
+            <Link href="/signup">Get Started</Link>
+          </Button>
+          <Button asChild variant="secondary">
+            <Link href="/login">Log in</Link>
+          </Button>
+        </div>
       </div>
     </main>
   );
